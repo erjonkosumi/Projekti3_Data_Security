04cdb5c3819e89e73d85963756cb30022e60f4e4
erjonkosumi <kosumierjon@gmail.com>
2022-05-15 12:12:16 +0200

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,1 +1,19 @@
+import numpy as np
+import math
 
+def Encrypt():
+    print("Enkriptimi nga Plain ne Cipher Text")
+    print("Ne rast se teksti nuk eshte i gjate mjaftueshem sa ta mbuloj celesi ateher mesazhit i shtohet nje Z")
+
+  #Inputi shendrrohet ne upper-case
+    mesazhi = input("Plain Text: ").upper()
+
+    #Validimi nuk i merr parasysh hapesirat ose karakteret speciale per shkak se punon vetem ne baze te rendit alfabetik (A-Z)
+    temp=""
+    for x in mesazhi:
+        try:
+            if x!=" ":
+                int(x)
+        except:
+            temp+=x
+    mesazhi=temp
