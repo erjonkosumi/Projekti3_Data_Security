472d099afc0cb41c3747fdb0603e073b9d8c1261
ErmalMillaku09 <95182356+ErmalMillaku09@users.noreply.github.com>
2022-05-15 14:14:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,21 @@ def cls():
         os.system("cls")
     else:
         os.system("clear")
+        
+ print("Hill Cipher Converter Pro")
+    print("━━━━━━━━━━━━━━━━━━━━━━━━━")
+def main():
+    cls()
+    exit=False
+    print("Welcome to my program :)")
+    input("Enter...")
+    while True:
+        try:
+            cls()
+            print("1. Hill Cipher Encoder\n2. Exit")
+            n=int(input("\nChoose Program : "))
+            if n==1:
+                cls()
+                Encrypt()
+         
+           
